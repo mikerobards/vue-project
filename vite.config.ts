c6236e38ec66e4da40004a11ce27d474551513f6
Michael Robards <mikerobards@icloud.com>
2023-07-09 09:14:31 -0400

    add plugins, font, bg

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Unfonts from 'unplugin-fonts/vite'
+import Components from 'unplugin-vue-components/vite'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Unfonts({
+      // Google Fonts API V2
+      google: {
+        preconnect: true,
+        display: 'block',
+        families: [
+          'Quicksand',
+        ],
+      },
+    }),
+    Components(),
   ],
   resolve: {
     alias: {
